aa0092138b33a451c96dee5a147fc1185552cc43
cmrd23-code <cmrd23@gmail.com>
2026-06-30 19:04:50 +0530

    Update Book.jsx

--- src/Book.jsx
+++ src/Book.jsx
@@ -7,7 +7,7 @@ function Book({b}) {
       <img src={b.image_url}></img>
       <p>Title: {b.book_title}</p>
       <p>Author:{b.book_author}</p>
-      <p>Price:{b.publisher}</p>
+      <p>Publisher:{b.publisher}</p>
       <p>Year of Publication:{b.year_of_publication}</p>
       <p>Price:{b.price}</p>
       <p>Pages:{b.pages}</p>
