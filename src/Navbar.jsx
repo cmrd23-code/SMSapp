fadc4fc050ca50e66b3c09c0ce2f24ce2c787f7d
cmrd23-code <cmrd23@gmail.com>
2026-06-30 17:00:06 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,25 +3,15 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar">
       <div className="logo">Student Performance Tracker</div>
 
-      <ul className="nav-items">
-        <li  className="nav-items">
-          <a className="nav-links" href="/">Home</a>
-        </li>
-        <li className="nav-items">
-          <a className="nav-links" href="/users">Students</a>
-        </li>
-        <li className="nav-items">
-          <a className="nav-links" href="/products">Products</a>
-        </li>
-        <li className="nav-items">
-          <a className="nav-links" href="/about">About</a>
-        </li>
-        <li className="nav-items">
-          <a className="nav-links" href="/contact">Contact</a>
-        </li>
+      <ul className="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/users">Students</a></li>
+        <li><a href="/products">Products</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
     </nav>
   );
